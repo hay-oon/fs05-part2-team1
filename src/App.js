4d5ea5f64f67950f48ac8bd51faa79fe968159bb
Syyga <gsh2672@gmail.com>
2025-01-13 14:26:14 +0900

    123

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Seha from "koseha";
 import Hayoon from "./hay-oon/page";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import Syga from "syyga/Syga";
+import Military from "syyga/Millitary";
+import SygaHistory from "syyga/SygaHistory";
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
             <Route path="/" element={<HeroSection />} />
             <Route path="/seha" element={<Seha />} />
             <Route path="/hay-oon" element={<Hayoon />} />
+            <Route path="/syga" element={<Syga />} />
+            <Route path="/millitary" element={<Military />} />
+            <Route path="/intro" element={<SygaHistory />} />
           </Routes>
         </main>
         <Footer />
